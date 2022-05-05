d2272894dda18eb5653e8a5983742aa91b94e096
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2022-05-05 15:37:01 -0300

    route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
 import express from "express";
+import { send } from "process";
+import { prisma } from "./prisma";
 
 const app = express();
+app.use(express.json()); // Isso é chamado de midleware
 
-app.get("/", (req, res) => {
-    res.send("Toma essa rota");
+/* 
+Métodos API
+GET = Buscar Informações
+POST = Gravar informações
+PUT = Atualizar informações de uma entidade (Varias 
+    colunas de uma linha, ex.: Nome, Idade, Endereço)
+PATCH = Atualizar uma informação unica de uma entidade
+    (No exemplo acima seria como atualizar somente o nome)
+DELETE = Deletar uma informação
+*/
+
+app.get("/users", (req, res) => {
+    return res.send("Teste");
 });
-app.get("/clientes", (req, res) => {
-    res.send("Toma essa também dada");
+
+app.post("/feedbacks", async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        },
+    });
+
+    return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => console.log("tá rodando fdp"));
